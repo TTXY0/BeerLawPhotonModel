e9e40ecfbb5a5849cfa17e9550368dee75ce602b
TTXY0 <98242277+TTXY0@users.noreply.github.com>
2023-07-27 16:18:18 -0500

    fixed function, edited test file

--- mu_to_p0.py
+++ mu_to_p0.py
@@ -1,24 +1,32 @@
 import numpy as np
-def mu_to_p0(mu, source, h): #mu is an array of attenuation coefficients, source is a tuple containing the x and y coordinates, h is the spacing between sampling points along the ray
+def mu_to_p0(mu, source, h, xp: np.array, yp: np.array): #mu is an array of attenuation coefficients, source is a tuple containing the x and y coordinates, h is the spacing between sampling points along the ray
     xs, ys = source
+    dpx  = xp[1]-xp[0] #spacing between sampling points
+    dpy = yp[1]-yp[0]
     P = np.zeros_like(mu)
     
-    for yi in range(mu.shape[0]):
-        for xi in range(mu.shape[1]):
-            m = (ys - yi) / (xs - xi)
-            b = ys - m * xs
+    pixelsx = []
+    pixelsy = []
+    
+    for index_y in range(mu.shape[0]):
+        for index_x in range(mu.shape[1]):
+            xi = xp[index_x] #physical coordinates
+            yi = yp[index_y]
             
             d = ((xi-xs)**2 + (yi-ys)**2) **0.5 #euclidean distance between source and target
             n = int(d/h) + 1 # of discrete point
-            
-            dx =  (xi - xs) / (n - 1) #Fix? There are points outside the image dimensions
+
+            dx =  (xi - xs) / (n - 1) #physical space dx
             dy = (yi - ys) / (n - 1)
             
             for point_i in range(n):
-                x = int(np.floor(xs + (point_i * dx))) #The pixel at which the point is located
-                y = int(np.floor(ys + (point_i * dy))) # ^
-
-                if 0 <= x < mu.shape[0] and 0 <= y < mu.shape[1]:
-                    P[y,x] += mu[y,x]
+                pixelsx.append((xs + (point_i * dx) - xp[0])/ (dpx) )#pixel indices
+                pixelsy.append((ys + (point_i * dy) - yp[0])/ (dpy))
                 
-    return mu * np.exp(-P), P
+                
+                i_x = int(np.floor(xs + (point_i * dx) - xp[0])/ (dpx)) #pixel indices
+                i_y = int(np.floor(ys + (point_i * dy) - yp[0])/ (dpy)) 
+                if 0 <= i_x < mu.shape[0] and 0 <= i_y < mu.shape[1]:
+                    P[i_y,i_x] += mu[i_y,i_x] * (h)
+                
+    return mu * np.exp(-P), P, pixelsx, pixelsy
--- test.py
+++ test.py
@@ -3,19 +3,25 @@ import matplotlib.pyplot as plt
 import mu_to_p0
 
 
-def gauss_density_pattern(size_x, size_y, amplitude, sigma):
-    x, y = np.meshgrid(np.arange(size_x), np.arange(size_y))
-    x0, y0 = size_x // 2, size_y // 2  # Center of the ball
-    density = amplitude * np.exp(-((x - x0)**2 + (y - y0)**2) / (2 * sigma**2))
-    return np.array(density)
+# def gauss_density_pattern(size_x, size_y, amplitude, sigma):
+#     x, y = np.meshgrid(np.arange(size_x), np.arange(size_y))
+#     x0, y0 = size_x // 2, size_y // 2  # Center of the ball
+#     density = amplitude * np.exp(-((x - x0)**2 + (y - y0)**2) / (2 * sigma**2))
+#     return np.array(density)
 
-mu = gauss_density_pattern(100, 100, 50, 20)
+# mu = gauss_density_pattern(100, 100, 50, 20)
 
-            
-P0, P = (mu_to_p0.mu_to_p0(mu, (-1,-1), .1))
+mu = np.ones((2,2), dtype=int)
+
+xp = np.linspace(0,1,2)
+yp = np.linspace(0,1,2)
+print(xp, yp)
+
+P0, P, pixelsx, pixelsy= mu_to_p0.mu_to_p0(mu, (-1 ,-1), 1, xp, yp)
 
 
 fig, ax = plt.subplots(1, 3, figsize=(12, 4))
+ax[1].scatter(pixelsx, pixelsy)
 ax[0].set_title("Attenuation Matrix")
 ax[1].set_title("'P'")
 ax[2].set_title("P0")
