6bbf374ed5aca91a1fe0334a0dac49eae9f531c5
TTXY0 <twynn5403@gmail.com>
2023-07-31 19:38:14 -0500

    added isotropic cone implementation

--- mu_to_p0.py
+++ mu_to_p0.py
@@ -8,8 +8,8 @@ def mu_to_p0(mu, source, h, xp: np.array, yp: np.array): #mu is an array of atte
     dpy = yp[1]-yp[0]
     a = np.zeros_like(mu)
     
-    pixelsx = {}
-    pixelsy = {}
+    # pixelsx = {}
+    # pixelsy = {}
 
     for index_y in range(mu.shape[0]):
         for index_x in range(mu.shape[1]):
@@ -23,12 +23,12 @@ def mu_to_p0(mu, source, h, xp: np.array, yp: np.array): #mu is an array of atte
             dx =  (xi - xs) / (n - 1) #physical space dx
             dy = (yi - ys) / (n - 1)
             
-            ppx = [] 
-            ppy = []
+            # ppx = [] 
+            # ppy = []
                        
             for point_i in range(n):
-                ppx.append( (xs + point_i * dx - xp[0]  ) / dpx ) #pixel indices
-                ppy.append( (ys + point_i * dy - yp[0]  ) / dpy ) 
+                # ppx.append( (xs + point_i * dx - xp[0]  ) / dpx ) #pixel indices
+                # ppy.append( (ys + point_i * dy - yp[0]  ) / dpy ) 
                 
                 
                 i_x = int(np.floor( (xs + point_i * dx - xp[0] + 0.51*dpx ) / dpx ) ) #pixel indices
@@ -36,8 +36,53 @@ def mu_to_p0(mu, source, h, xp: np.array, yp: np.array): #mu is an array of atte
                 if 0 <= i_x < mu.shape[0] and 0 <= i_y < mu.shape[1]:
                     a[index_y, index_x] += mu[i_y,i_x] * h
                     
-            pixelsx[index_y, index_x] = ppx
-            pixelsy[index_y, index_x] = ppy
+            # pixelsx[index_y, index_x] = ppx
+            # pixelsy[index_y, index_x] = ppy
             
                 
-    return mu * np.exp(-a), a, pixelsx, pixelsy
+    return mu * np.exp(-a), a #, pixelsx, pixelsy
+
+
+def mu_to_p0_cone(mu, source, h, xp: np.array, yp: np.array, theta, direction): #mu is an array of attenuation coefficients, source is a tuple containing the x and y coordinates, h is the spacing between sampling points along the ray
+    xs, ys = source                                                                 #theta is the width of the "flashlight", direction is the "pointed" direction of the flashlight
+    assert mu.shape[1]==xp.shape[0]
+    assert mu.shape[0]==yp.shape[0]
+    
+    dpx  = xp[1] - xp[0] #spacing between sampling points
+    dpy = yp[1] - yp[0]
+    
+    a = np.zeros_like(mu)
+    
+    #Mask for "flashlight"
+    mask =  np.zeros_like(mu)
+    for yi in range(mask.shape[0]):
+        for xi in range(mask.shape[1]):
+            point_angle = np.arctan2 (yi - ys, xi - xs) - direction
+            if np.abs(point_angle) <= theta/2: #include max distance? 
+                mask[yi, xi] = 1
+
+    p0 = np.zeros_like(mu)
+    for index_y in range(mu.shape[0]):
+        for index_x in range(mu.shape[1]):
+            if mask[index_y , index_x] == 1:
+                
+                xi = xp[index_x] #physical coordinates
+                yi = yp[index_y]
+                
+                d = ((xi-xs)**2 + (yi-ys)**2) **0.5 #euclidean distance between source and target
+                n = int(d/h) + 1 # of discrete point
+
+                dx =  (xi - xs) / (n - 1) #physical space dx
+                dy = (yi - ys) / (n - 1)
+                
+                for point_i in range(n):
+                    i_x = int(np.floor( (xs + point_i * dx - xp[0] + 0.51*dpx ) / dpx ) ) #pixel indices
+                    i_y = int(np.floor( (ys + point_i * dy - yp[0] + 0.51*dpy ) / dpy ) ) 
+                    
+                    if 0 <= i_x < mu.shape[0] and 0 <= i_y < mu.shape[1]:
+                        #a[index_x, index_x] = .0
+                        a[index_y, index_x] += mu[i_y,i_x] * h
+                        
+                p0[index_y, index_x] = mu[index_y, index_x] * np.exp(-a[index_y, index_x])
+            
+    return p0, a, mask
--- test_cone.py
+++ test_cone.py
@@ -23,26 +23,24 @@ yc = np.linspace(-Ly/2 + dy/2, Ly/2 - dy/2, ny)
 
 mu = gauss_density_pattern(xc, yc, 50, Lx/10)
 # mu = np.ones((ny,nx), dtype=float)
+P0, a, mask = mu_to_p0.mu_to_p0_cone(mu, (-Lx/2, -Ly/2), dx/4, xc, yc, np.pi/4, np.pi/4)
 
-P0, a = mu_to_p0.mu_to_p0(mu, (-Lx/2, -Ly/2), dx/4, xc, yc)
+# print(a[0,0])
+# print(a[ny//2, nx//2])
 
+fig, ax = plt.subplots(1, 5, figsize=(12, 4))
 
-print(mu.shape, xc.shape, yc.shape)
-print(a[0,0])
-print(a[ny//2, nx//2])
+ax[0].set_title("Mask")
+ax[1].set_title("Attenuation Matrix")
+ax[2].set_title("'a'")
+ax[3].set_title("fluence")
+ax[4].set_title("P0")
 
-fig, ax = plt.subplots(1, 4, figsize=(12, 4))
-#ax[1].scatter(pixelsx[nx//2, ny//2], pixelsy[nx//2., ny//2])
-ax[0].set_title("Attenuation Matrix")
-ax[1].set_title("'a'")
-ax[2].set_title("fluence")
-ax[3].set_title("P0")
-
-ax[0].imshow(mu, cmap = "gray")
-ax[1].imshow(a, cmap = "gray")
-ax[2].imshow(np.exp(-a), cmap = "gray")
-#ax[3].imshow(mask, cmap = 'gray')
-ax[3].imshow(P0, cmap = "gray")
+ax[0].imshow(mask, cmap = "gray")
+ax[1].imshow(mu, cmap = "gray")
+ax[2].imshow(a, cmap = "gray")
+ax[3].imshow(np.exp(-a), cmap = "gray")
+ax[4].imshow(P0, cmap = "gray")
 
 
 
