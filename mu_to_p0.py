9492c265813e6f7078f58d42a974cbcc8cac403c
TTXY0 <98242277+TTXY0@users.noreply.github.com>
2023-08-09 12:51:49 -0500

    added 3d implementation and visualizations

--- mu_to_p0.py
+++ mu_to_p0.py
@@ -7,9 +7,6 @@ def mu_to_p0(mu, source, h, xp: np.array, yp: np.array): #mu is an array of atte
     dpx  = xp[1]-xp[0] #spacing between sampling points
     dpy = yp[1]-yp[0]
     a = np.zeros_like(mu)
-    
-    # pixelsx = {}
-    # pixelsy = {}
 
     for index_y in range(mu.shape[0]):
         for index_x in range(mu.shape[1]):
@@ -22,25 +19,15 @@ def mu_to_p0(mu, source, h, xp: np.array, yp: np.array): #mu is an array of atte
 
             dx =  (xi - xs) / (n - 1) #physical space dx
             dy = (yi - ys) / (n - 1)
-            
-            # ppx = [] 
-            # ppy = []
                        
             for point_i in range(n):
-                # ppx.append( (xs + point_i * dx - xp[0]  ) / dpx ) #pixel indices
-                # ppy.append( (ys + point_i * dy - yp[0]  ) / dpy ) 
-                
-                
+ 
                 i_x = int(np.floor( (xs + point_i * dx - xp[0] + 0.51*dpx ) / dpx ) ) #pixel indices
                 i_y = int(np.floor( (ys + point_i * dy - yp[0] + 0.51*dpy ) / dpy ) ) 
                 if 0 <= i_x < mu.shape[0] and 0 <= i_y < mu.shape[1]:
                     a[index_y, index_x] += mu[i_y,i_x] * h
-                    
-            # pixelsx[index_y, index_x] = ppx
-            # pixelsy[index_y, index_x] = ppy
-            
-                
-    return mu * np.exp(-a), a #, pixelsx, pixelsy
+                 
+    return mu * np.exp(-a), a 
 
 
 def mu_to_p0_cone(mu, source, h, xp: np.array, yp: np.array, theta, direction): #mu is an array of attenuation coefficients, source is a tuple containing the x and y coordinates, h is the spacing between sampling points along the ray
@@ -88,3 +75,97 @@ def mu_to_p0_cone(mu, source, h, xp: np.array, yp: np.array, theta, direction): 
                 p0[index_y, index_x] = mu[index_y, index_x] * np.exp(-a[index_y, index_x])
             
     return p0, a, mask
+
+
+def mu_to_p0_3d(mu, source, h, xp, yp, zp):
+    xs, ys, zs = source
+    assert mu.shape[2] == xp.shape[0]
+    assert mu.shape[1] == yp.shape[0]
+    assert mu.shape[0] == zp.shape[0]
+    
+    dpx = xp[1] - xp[0]
+    dpy = yp[1] - yp[0]
+    dpz = zp[1] - zp[0]
+    
+    a = np.zeros_like(mu)
+    
+    for index_z in range(mu.shape[0]):
+        for index_y in range(mu.shape[1]):
+            for index_x in range(mu.shape[2]):
+                xi = xp[index_x]
+                yi = yp[index_y]
+                zi = zp[index_z]
+                
+                d = ((xi - xs)**2 + (yi - ys)**2 + (zi - zs)**2)**0.5
+                n = int(d / h) + 1
+                
+                dx = (xi - xs) / (n - 1)
+                dy = (yi - ys) / (n - 1)
+                dz = (zi - zs) / (n - 1)
+                
+                for point_i in range(n):
+                    i_x = int(np.floor((xs + point_i * dx - xp[0] + 0.51 * dpx) / dpx))
+                    i_y = int(np.floor((ys + point_i * dy - yp[0] + 0.51 * dpy) / dpy))
+                    i_z = int(np.floor((zs + point_i * dz - zp[0] + 0.51 * dpz) / dpz))
+                    
+                    if 0 <= i_x < mu.shape[2] and 0 <= i_y < mu.shape[1] and 0 <= i_z < mu.shape[0]:
+                        a[index_z, index_y, index_x] += mu[i_z, i_y, i_x] * h
+                    
+    return mu * np.exp(-a), a
+
+def mu_to_p0_cone_3d(mu, source, h, xp: np.array, yp: np.array, zp: np.array, direction_vector, theta):
+    xs, ys, zs = source
+    
+    assert mu.shape[2] == xp.shape[0]
+    assert mu.shape[1] == yp.shape[0]
+    assert mu.shape[0] == zp.shape[0]
+    
+    dpx = xp[1] - xp[0]   
+    dpy = yp[1] - yp[0] 
+    dpz = zp[1] - zp[0]   
+    
+    a = np.zeros_like(mu)
+    
+    mask = np.zeros_like(mu)
+    for index_z in range(mask.shape[0]):
+        for index_y in range(mask.shape[1]):
+            for index_x in range(mask.shape[2]):
+                xi = xp[index_x]
+                yi = yp[index_y]
+                zi = zp[index_z]
+                
+                vector_shift = np.array([xi - xs, yi - ys, zi - zs])
+                 
+                point_angle = np.arccos(   np.dot(direction_vector, vector_shift)  /  (np.linalg.norm(direction_vector) * np.linalg.norm(vector_shift)   )   )
+                if point_angle <= theta / 2:
+                    mask[index_z, index_y, index_x] = 1
+
+    p0 = np.zeros_like(mu)
+    for index_z in range(mu.shape[0]):
+        for index_y in range(mu.shape[1]):
+            for index_x in range(mu.shape[2]):
+                if mask[index_z, index_y, index_x] == 1:
+                    xi = xp[index_x]
+                    yi = yp[index_y]
+                    zi = zp[index_z]
+                    
+                    d = ((xi - xs)**2 + (yi - ys)**2 + (zi - zs)**2)**0.5
+                    n = int(d / h) + 1
+                    
+                    dx = (xi - xs) / (n - 1)
+                    dy = (yi - ys) / (n - 1)
+                    dz = (zi - zs) / (n - 1)
+                    
+                    for point_i in range(n):
+                        i_x = int(np.floor((xs + point_i * dx - xp[0] + 0.51 * dpx) / dpx))
+                        i_y = int(np.floor((ys + point_i * dy - yp[0] + 0.51 * dpy) / dpy))
+                        i_z = int(np.floor((zs + point_i * dz - zp[0] + 0.51 * dpz) / dpz))
+                        
+                        if 0 <= i_x < mu.shape[2] and 0 <= i_y < mu.shape[1] and 0 <= i_z < mu.shape[0]:
+                            a[index_z, index_y, index_x] += mu[i_z, i_y, i_x] * h
+                        
+                    p0[index_z, index_y, index_x] = mu[index_z, index_y, index_x] * np.exp(-a[index_z, index_y, index_x])
+            
+    return p0, a, mask
+
+
---
+++ test_cone_3d.py
@@ -0,0 +1,107 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mu_to_p0
+import plotly.graph_objs as go
+
+
+def gauss_density_pattern(xp, yp, zp, amplitude, sigma):
+    x, y, z = np.meshgrid(xp, yp, zp)
+    x0 = xp.mean()
+    y0 = yp.mean()
+    z0 = zp.mean()
+    density = amplitude * np.exp(-((x - x0)**2 + (y - y0)**2 + (z - z0)**2) / (2 * sigma**2))
+    return density
+
+Lx = 2
+Ly = 2
+Lz = 2
+
+nx = 16
+ny = 16
+nz = 16
+
+dx = Lx / nx
+dy = Ly / ny
+dz = Lz / nz
+
+xc = np.linspace(-Lx/2 + dx/2, Lx/2 - dx/2, nx)
+yc = np.linspace(-Ly/2 + dy/2, Ly/2 - dy/2, ny)
+zc = np.linspace(-Lz/2 + dz/2, Lz/2 - dz/2, nz)
+
+mu = gauss_density_pattern(xc, yc, zc, 50, Lx/10)
+
+source = (-Lx/2, -Ly/2, -Lz/2)
+
+P0, a, mask = mu_to_p0.mu_to_p0_cone_3d(mu, source, dx/2, xc, yc, zc, [1,1,1], np.pi/10)
+
+# #P0
+X, Y, Z = np.meshgrid(xc, yc, zc)
+marker_size = 30 * P0 / np.max(P0)
+P0_Scatter = go.Scatter3d(x=X.flatten(), y=Y.flatten(), z=Z.flatten(), 
+                                   mode='markers', 
+                                   marker=dict(size=marker_size.flatten(),
+                                               color=P0.flatten(),
+                                               colorscale='gray',
+                                               opacity=1,
+                                               colorbar=dict(title='Value')
+                                   ))
+layout = go.Layout(scene=dict(aspectmode='data'), title = "P0")
+fig0 = go.Figure(data=[P0_Scatter], layout=layout)
+fig0.show()
+
+#Alpha
+marker_size = 30 * a / np.max(a)
+P0_Scatter = go.Scatter3d(x=X.flatten(), y=Y.flatten(), z=Z.flatten(), 
+                                   mode='markers', 
+                                   marker=dict(size=marker_size.flatten(),
+                                               color=a.flatten(),
+                                               colorscale='gray',
+                                               opacity=1,
+                                               colorbar=dict(title='Value')
+                                   ))
+layout = go.Layout(scene=dict(aspectmode='data'), title= "Alpha")
+fig1 = go.Figure(data=[P0_Scatter], layout=layout)
+fig1.show()
+
+#Fluence
+fluence = np.exp(-a)
+marker_size = 30 * fluence / np.max(fluence)
+P0_Scatter = go.Scatter3d(x=X.flatten(), y=Y.flatten(), z=Z.flatten(), 
+                                   mode='markers', 
+                                   marker=dict(size=marker_size.flatten(),
+                                               color=fluence.flatten(),
+                                               colorscale='viridis',
+                                               opacity=1,
+                                               colorbar=dict(title='Value')
+                                   ))
+layout = go.Layout(scene=dict(aspectmode='data'), title= "Fluence")
+fig2 = go.Figure(data=[P0_Scatter], layout=layout)
+fig2.show()
+
+
+#mask
+marker_size = 30 * mask / np.max(mask)
+P0_Scatter = go.Scatter3d(x=X.flatten(), y=Y.flatten(), z=Z.flatten(), 
+                                   mode='markers', 
+                                   marker=dict(size=marker_size.flatten(),
+                                               color=mask.flatten(),
+                                               colorscale='gray',
+                                               opacity=1,
+                                               colorbar=dict(title='Value'))
+                                   )
+layout = go.Layout(scene=dict(aspectmode='data'), title= "Mask")
+fig3 = go.Figure(data=[P0_Scatter], layout=layout)
+fig3.show()
+
+
+
+
+P0_2d = np.sum(mask, axis=0)
+plt.figure(figsize=(8, 6))
+plt.imshow(P0_2d, cmap='gray', extent=(xc[0], xc[-1], yc[0], yc[-1]), origin='lower')
+plt.colorbar(label='Attenuation')
+
+plt.title("2D Projection of P0")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
